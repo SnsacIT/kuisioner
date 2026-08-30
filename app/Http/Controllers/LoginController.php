b79cf76ca93aef125ee1e3e07921270f77274c18
Dhiaurrahman Raziq Ramadhan <ramadhanraziq@gmail.com>
2026-08-30 09:26:34 +0700

    update nip

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -299,7 +299,8 @@ class LoginController extends Controller
             '2011170077',
             '2209470326',
             '0126071448',
-            '0126041376'
+            '0126041376',
+            '2203360226'
         ]) || $checkCabang)) {
             return back()->withErrors([
                 'nip' => 'Anda tidak memiliki akses.',
